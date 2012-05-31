3dc6d19f8bd44926e9733453928316ca8ecd7a79
Juzer Ali <er.juzerali@gmail.com>
2012-06-01 00:18:43 +0530

    added all the tests

--- Tests.js
+++ Tests.js
@@ -1,16 +1,12 @@
 var should = chai.should();
 describe("Sales Tax example", function() {
-
+  afterEach(function(){
+    console.log(bill);
+  });
 
   describe("when 'input' 1 is fed", function() {
-    it("should return Total: 29.83, Sales Taxes: 1.50", function() {
+    it("should return Total: 29.83, Sales Taxes: 1.50 woth correct individual prices", function() {
 
-    	/*
-    	*  Input 1:
-  		*	1 book at 12.49
-  		*	1 music CD at 14.99
-  		*	1 chocolate bar at 0.85
-  		*/
       var input1 = [
       	{name: "1 book", price: 12.49, type: "book", imported: false},
       	{name: "1 music CD", price: 14.99, type: "other", imported: false},
@@ -22,35 +18,43 @@ describe("Sales Tax example", function() {
       	bill["Sales Taxes"].should.equal(1.5);
     });
   });
-/*
+
+
   describe("when 'input' 2 is fed", function() {
-    it("should return output", function() {
-  /* 1 imported bottle of perfume: 32.19
-  * 1 bottle of perfume: 20.89
-  * 1 packet of headache pills: 9.75
-  * 1 imported box of chocolates: 11.85
-  */
+    it("should return Total: 65.15, Sales Tax: 7.65", function() {
+
+      var input2 = [
+            {name: "1 imported box of chocolates", price: 10.00, type: "food", imported: true},
+            {name: "1 imported bottle of perfume", price: 47.50, type: "other", imported: true}
+          ];
+
+      var bill = new Bill(input2);
+      bill.should.be.a("object");
+      bill["1 imported box of chocolates"].should.equal(10.5);
+      bill["1 imported bottle of perfume"].should.equal(54.65);
+      bill["Sales Taxes"].should.equal(7.65);
+      bill["Total"].should.equal(65.15);
+    });
+  });
 
 
-/*
-  Input 2: 1 imported box of chocolates at 10.00 1 imported bottle of perfume at 47.50
+  describe("when 'input' 3 is fed", function() {
+    it("should return Total: 65.15, Sales Tax: 7.65", function() {
 
-  var input2 = [
-        {name: "1 imported box of chocolates at 10.00", price: 10.00, type: "food", imported: true},
-        {name: "1 imported bottle of perfume at 47.50", price: 47.50, type: "other", imported: true}
+      var input3 = [
+        {name: "1 imported bottle of perfume", price: 32.19, type: "other", imported: true},
+        {name: "1 bottle of perfume", price: 20.89, type: "other", imported: false},
+        {name: "1 packet of headache pills", price: 9.75, type: "medicine", imported: false},
+        {name: "1 imported box of chocolates", price: 11.85, type: "food", imported: true}
       ];
 
-  1 imported bottle of perfume: 32.19
-  1 bottle of perfume: 20.89
-  1 packet of headache pills: 9.75
-  1 imported box of chocolates: 11.85
-
-  var input3 = [
-        {name: "1 imported bottle of perfume: 32.19", price: 32.19, type: "other", imported: true},
-        {name: "1 bottle of perfume 20.89", price: 20.89, type: "other", imported: false},
-        {name: "1 packet of headache pills: 9.75", price: 9.75, type: "medicine", imported: false},
-        {name: "1 imported box of chocolates: 11.85", price: 11.85, type: "food", imported: true}
-      ];
-}*/
-
+      var bill = new Bill(input3);
+      bill.should.be.a("object");
+      bill["1 imported bottle of perfume"].should.equal(32.19);
+      bill["1 bottle of perfume"].should.equal(20.89);
+      bill["1 packet of headache pills"].should.equal(11.85);
+      bill["Sales Taxes"].should.equal(6.70);
+      bill["Total"].should.equal(74.68);
+    });
+  });
 });
