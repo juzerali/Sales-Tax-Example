8c2530c67ffac195c5f7089ebe6be9fe4097d9f6
Juzer Ali <er.juzerali@gmail.com>
2012-05-31 08:51:54 +0530

    salestax-example.js complete

--- Tests.js
+++ Tests.js
@@ -5,10 +5,10 @@ describe("Sales Tax example", function() {
     it("Output should be", function() {
     	/*
     	*   Input 1:
-		*	1 book at 12.49
-		*	1 music CD at 14.99
-		*	1 chocolate bar at 0.85
-		*/
+  		*	1 book at 12.49
+  		*	1 music CD at 14.99
+  		*	1 chocolate bar at 0.85
+  		*/
       var input1 = [
       	{name: "1 book at 12.49", price: 12.49, type: "book", imported: false},
       	{name: "1 music CD at 14.99", price: 14.99, type: "other", imported: false},
@@ -19,26 +19,23 @@ describe("Sales Tax example", function() {
       	bill.SalesTax.should.equal("1.50");
     });
   });
-/*
-  describe("when a number is passed in", function() {
-    it("returns the number", function() {
-      var result = StringCalculator.add("2");
-      assert(result === 2);
-    });
-  });
 
-  describe("when string is passed in", function() {
-    it("returns NaN", function() {
-      var result = StringCalculator.add("a");
-      assert(isNaN(result));
-    });
-  });
+  Input 2: 1 imported box of chocolates at 10.00 1 imported bottle of perfume at 47.50
 
-  describe("when '1,2' is passed in", function() {
-    it("returns 3", function() {
-      var result = StringCalculator.add("1,2");
-      assert(result === 3);
-    });
-  });
-});
-*/
+  var input2 = [
+        {name: "1 imported box of chocolates at 10.00", price: 10.00, type: "food", imported: true},
+        {name: "1 imported bottle of perfume at 47.50", price: 47.50, type: "other", imported: true}
+      ];
+
+  1 imported bottle of perfume: 32.19
+  1 bottle of perfume: 20.89
+  1 packet of headache pills: 9.75
+  1 imported box of chocolates: 11.85
+
+  var input3 = [
+        {name: "1 imported bottle of perfume: 32.19", price: 32.19, type: "other", imported: true},
+        {name: "1 bottle of perfume 20.89", price: 20.89, type: "other", imported: false},
+        {name: "1 packet of headache pills: 9.75", price: 9.75, type: "medicine", imported: false},
+        {name: "1 imported box of chocolates: 11.85", price: 11.85, type: "food", imported: true}
+      ];
+}
--- salestax-example.js
+++ salestax-example.js
@@ -2,10 +2,10 @@ var Tax = function(salesTaxValue, importTaxValue){
 	var salesTax = salesTaxValue || 10;
 	var importTax = importTaxValue || 5;
 
-	this.exempted = ["food", "books", "medicines"]
+	this.exempted = ["food", "book", "medicine"]
 
 	this.getSalesTax = function(type){
-		if(this.exempted.indexOf(type) == -1) return 0;
+		if(this.exempted.indexOf(type) != -1) return 0;
 		return salesTax;
 	};
 
@@ -22,8 +22,8 @@ var Tax = function(salesTaxValue, importTaxValue){
 	};
 
 	this.calculate = function(item){
-		taxApplicable = this.getSalesTax(item.type) + this.getImportTax(item.imported);
-		return (Math.round((item.price*taxApplicable/100)*100/5)*5/100).toFixed(2);
+		item.taxApplicable = this.getSalesTax(item.type) + this.getImportTax(item.imported);
+		return (Math.round((item.price*item.taxApplicable/100)*100/5)*5/100);
 	};
 }
 
@@ -31,15 +31,15 @@ var tax = new Tax();
 
 var BillGenerator = function(bill){
 	var billDetails = {};
-	billDetails["Sales Taxes"] = 0;
-	$.each(bill, function(index, item){
-		console.log(JSON.stringify(item));
-		billDetails[item["name"]] = Number(item["price"]).toFixed(2);
+	billDetails["Sales Taxes"] = billDetails["Total"] = 0;
+	bill.forEach(function(item){
 		var taxed = tax.calculate(item);
-		billDetails["Sales Taxes"] += Number(taxed);
-		billDetails["Total"] += (Number(billDetails[item["name"]])+taxed);
+		billDetails["Sales Taxes"] += taxed;
+		billDetails["Total"] = billDetails["Total"] + item["price"] + taxed;
+
+		billDetails[item["name"]] = Number(item["price"]).toFixed(2);
 	});
-	//billDetails["Total"] = billDetails["Total"].toFixed(2);
-	//billDetails["Sales Taxes"] = billDetails["Sales Taxes"].toFixed(2);
+	billDetails["Total"] = billDetails["Total"].toFixed(2);
+	billDetails["Sales Taxes"] = billDetails["Sales Taxes"].toFixed(2);
 	return billDetails;
 };
